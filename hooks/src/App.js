a00cd6f7c79c9a613b62539c6955fe2a6c7febd4
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-27 00:46:41 +0530

    update

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Counter from './components/Counter';
 import Counter2 from './components/Counter2';
 import Counter3 from './components/Counter3';
+import Counter4 from './components/Counter4';
 import Hooks1 from './components/Hooks1';
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
      <Counter2/>
      <br></br>
      <Counter3/>
+     <br></br>
+     <Counter4/>
     </div>
   );
 }
