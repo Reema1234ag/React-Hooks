cf7b51abfb196d5bdb303e41872c76dd4edc1a2c
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-26 23:06:28 +0530

    update

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Counter from './components/Counter';
+import Counter2 from './components/Counter2';
+import Counter3 from './components/Counter3';
 import Hooks1 from './components/Hooks1';
 
 function App() {
   return (
     <div className="App">
      <Hooks1/> 
+     <Counter/>
+     <hr></hr>
+     <Counter2/>
+     <br></br>
+     <Counter3/>
     </div>
   );
 }
---
+++ hooks/src/components/Counter3.js
@@ -0,0 +1,11 @@
+//object as a state variable with the useState hook
+import React from 'react'
+
+export default function Counter3() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+ 
