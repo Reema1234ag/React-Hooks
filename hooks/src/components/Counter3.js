3a6016c60cbff19dfb22e0a29b0a411d580d4d8a
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-27 00:04:14 +0530

    useState with object

--- hooks/src/components/Counter3.js
+++ hooks/src/components/Counter3.js
@@ -6,12 +6,12 @@ export default function Counter3() {
     return (
         <div>
             <form>
-            <input type='text' value={name.firstname} onChange={e=>{setname({firstname:e.target.value})}}/>     
-            <input type='text' value={name.lastname} onChange={e=>{setname({lastname:e.target.value})}}/>
+            <input type='text' value={name.firstname} onChange={e=>{setname({...name,firstname:e.target.value})}}/>   {/*using spread operator */}
+            <input type='text' value={name.lastname} onChange={e=>{setname({...name,lastname:e.target.value})}}/>
             <h2>Your firstname is- {name.firstname}</h2>
             <h2>Your Lastname is- {name.lastname}</h2>
             <h2>{JSON.stringify(name)}</h2>             {/* useState donot automaticaly merge and update the object where setState in class component does
-                                                                and this is the key difference in useState and setState */}
+            and this is the key difference in useState and setState */}
             </form>
         </div>
     )
