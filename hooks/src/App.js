df4b28d0a47eb3ee7c6c3a3e515ca8fa91a43e12
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-05-27 22:15:55 +0530

    updated

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import IncrementPerSec1 from './components/class component/IncrementPerSec1';
 import Lifecycle from './components/class component/Lifecycle';
+import Lifecycle2 from './components/class component/Lifecycle2';
+import IncrementPerSec from './components/Common problems/IncrementPerSec';
 import Counter from './components/Counter';
 import Counter2 from './components/Counter2';
 import Counter3 from './components/Counter3';
 import Counter4 from './components/Counter4';
 import Hooks1 from './components/Hooks1';
 import EffectCounter from './components/useEffectHook/EffectCounter';
+import EffectCounter2 from './components/useEffectHook/EffectCounter2';
+import EffectReturn from './components/useEffectHook/EffectReturn';
 
 function App() {
   return (
@@ -21,10 +26,20 @@ function App() {
      <Counter4/>
      <hr></hr>
      <h1>useEffect hook</h1>
+     <h2>Example-1</h2>
      <Lifecycle/>
      <h3>(using class component)</h3>
      <EffectCounter/>
      <h3>(above using effect hook in functional component)</h3>
+     <h2>Example-2</h2>
+     <Lifecycle2/><h3>(using class component)</h3>
+     <EffectCounter2/><h3>(above using effect hook in functional component)</h3>
+     <br></br> 
+    <EffectReturn/>
+    <hr></hr>
+    <h2>Incrementing value in each second</h2>
+    <IncrementPerSec1/><h3>(using class component)</h3>
+    <IncrementPerSec/><h3>(using effect hook in functional component)</h3>
     </div>
   );
 }
